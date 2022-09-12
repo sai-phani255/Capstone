d5f1bd4d471ccacc192cd883d4206065086d99ad
Ananda Datta Sai Phanindra Tangirala <63234243+sai-phani255@users.noreply.github.com>
2022-09-12 18:24:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from flask import Flask, render_template, request
 #import jsonify
 import requests
@@ -25,50 +26,62 @@ def Home():
 def classify():
 
     if request.method == 'POST':
-        Segment_ = request.form['segment_']
-        Segment_ = segment_encoder.transform([Segment_])
+        Segment = request.form['segment_']
+        Segment_ = segment_encoder.transform([Segment])
 
-        Market_ = request.form['market_']
-        Market_ = market_encoder.transform([Market_])
+        Market = request.form['market_']
+        Market_ = market_encoder.transform([Market])
 
-        Region_ = request.form['region_']
-        Region_ = region_encoder.transform([Region_])
+        Region = request.form['region_']
+        Region_ = region_encoder.transform([Region])
 
-        Category_ = request.form['category_']
-        Category_ = category_encoder.transform([Category_])
+        Category = request.form['category_']
+        Category_ = category_encoder.transform([Category])
 
-        OrderPriority_ = request.form['order_priority_']
-        OrderPriority_ = order_priority_encoder.transform([OrderPriority_])
+        OrderPriority = request.form['order_priority_']
+        OrderPriority_ = order_priority_encoder.transform([OrderPriority])
 
 
         Costprice = round(float(request.form['Cost_Price']),3)
-        Costprice = np.log(Costprice)
+        Costprice_ = np.log(Costprice)
 
         Quantity = int(request.form['Quantity'])
 
         DiscountPercent = round(float(request.form['Discount_Percent']),2)
 
         ShippingCost = round(float(request.form['Shipping_Cost']),2)
-        ShippingCost = np.log(ShippingCost)
+        ShippingCost_ = np.log(ShippingCost)
 
         DaysToShip = int(request.form['Days_to_Ship'])
 
 
-        prediction = model.predict([[Segment_,Market_,Region_,Category_,OrderPriority_,Costprice,Quantity,
-                                    DiscountPercent,ShippingCost,DaysToShip]])
-
+        prediction = model.predict([[Segment_,Market_,Region_,Category_,OrderPriority_,Costprice_,Quantity,
+                                    DiscountPercent,ShippingCost_,DaysToShip]])
         
 
         if prediction==1:
-            return render_template('index.html',prediction_text="Highly Contributing")
+            return render_template('result.html',prediction_text="Highly Contributing",
+            costprice=Costprice,quantity=Quantity,disc_percent=DiscountPercent,shippingcost=ShippingCost,
+            daystoship=DaysToShip,segment=Segment,market=Market,region=Region,category=Category,
+            orderpriority=OrderPriority)
+
         elif prediction==2:
-            return render_template('index.html',prediction_text="Above Average Contributing")
+            return render_template('result.html',prediction_text="Above Average Contributing",
+            costprice=Costprice,quantity=Quantity,disc_percent=DiscountPercent,shippingcost=ShippingCost,
+            daystoship=DaysToShip,segment=Segment,market=Market,region=Region,category=Category,
+            orderpriority=OrderPriority)
         elif prediction==3:
-            return render_template('index.html',prediction_text="Below Average Contributing")
+            return render_template('result.html',prediction_text="Below Average Contributing",
+            costprice=Costprice,quantity=Quantity,disc_percent=DiscountPercent,shippingcost=ShippingCost,
+            daystoship=DaysToShip,segment=Segment,market=Market,region=Region,category=Category,
+            orderpriority=OrderPriority)
         else:
-            return render_template('index.html',prediction_text="Least Contributing")
-    else:
-        return render_template('index.html')
+            return render_template('result.html',prediction_text="Least Contributing",
+            costprice=Costprice,quantity=Quantity,disc_percent=DiscountPercent,shippingcost=ShippingCost,
+            daystoship=DaysToShip,segment=Segment,market=Market,region=Region,category=Category,
+            orderpriority=OrderPriority)
+    #else:
+    #    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(debug=False)
